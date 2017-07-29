a4dedf51b7ff858eeb72bb9e90b0e553467fefe4
kk6665555 <49990115@stust.edu.tw>
2017-07-29 14:49:42 +0800

    20170729

---
+++ Web051.php
@@ -0,0 +1,11 @@
+<?php
+    header('Content-Type: text/xml; charset="UTF-8"');
+?>
+<note>
+  <to>資策會</to>
+  <from>Jani</from>
+  <heading>Reminder</heading>
+  <body>Don't forget me this weekend!</body>
+  <to>歐1</to>
+  <to>歐2</to>
+</note>
